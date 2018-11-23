5368d10cd250d11d5c14a298928b47399c71eba0
Benjamin Morel <benjamin.morel@gmail.com>
2018-11-23 13:00:19 +0100

    Move Packed masks to 1-based array constants

--- src/Packer.php
+++ src/Packer.php
@@ -7,6 +7,16 @@ namespace BenMorel\GsmCharsetConverter;
 class Packer
 {
     /**
+     * The masks to keep only the first n bits of a byte, zeroing out the other bits.
+     */
+    const MASK_FIRST_N_BITS = [1 => 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE];
+
+    /**
+     * The masks to keep only the last n bits of a byte, zeroing out the other bits.
+     */
+    const MASK_LAST_N_BITS = [1 => 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F];
+
+    /**
      * Packs a 7-bit string into a 8-bit string.
      *
      * @param string $string
@@ -25,9 +35,6 @@ class Packer
         // We'll use put the latter and the former (in this order) together to form an octet.
         $bits = 7;
 
-        // The masks to keep only the last n bits of a byte.
-        $masks = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F];
-
         for ($i = 0; $i < $length; $i++) {
             $septet = ord($string[$i]);
 
@@ -42,7 +49,7 @@ class Packer
             }
 
             $octet = ($septet >> (7 - $bits));
-            $octet |= (($nextSeptet & $masks[8 - $bits - 1]) << $bits);
+            $octet |= (($nextSeptet & self::MASK_LAST_N_BITS[8 - $bits]) << $bits);
 
             $result .= chr($octet);
 
@@ -74,28 +81,22 @@ class Packer
         $length = strlen($string);
 
         // The number of bits we'll read from the right of the current octet.
-        // We'll carry the next (8 - $bits) bits onto the next septet.
+        // We'll carry the remaining (8 - $bits) bits onto the next septet.
         // We'll use put the former and the carry (in this order) together to form a septet.
         $bits = 7;
-
-        // The masks to keep only the last n bits of a byte.
-        $masksLast = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F];
-
-        // The masks to keep only the first n bits of a byte.
-        $masksFirst = [0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE];
 
         $carry = 0;
 
         for ($i = 0; $i < $length; $i++) {
             $octet = ord($string[$i]);
 
-            $septet = ($octet & $masksLast[$bits - 1]) << (7 - $bits);
+            $septet = ($octet & self::MASK_LAST_N_BITS[$bits]) << (7 - $bits);
 
             if ($bits !== 7) {
                 $septet |= ($carry >> ($bits + 1));
             }
 
-            $carry = ($octet & $masksFirst[8 - $bits - 1]);
+            $carry = ($octet & self::MASK_FIRST_N_BITS[8 - $bits]);
 
             $result .= chr($septet);
 
